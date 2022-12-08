ebac73a0463db32fd0275c2964b2678a4336e3d1
Muzaffer Kahraman <muzaffer.kahraman@outlook.com>
2022-12-08 12:26:53 +0300

    Update configure_hosts.sh

--- configure_hosts.sh
+++ configure_hosts.sh
@@ -1,7 +1,7 @@
 docker exec -ti clab-muzolab-host10 ip li set dev eth1 mtu 1500
 docker exec -ti clab-muzolab-host10 ip li set dev eth1 address 00:00:00:01:01:00
-docker exec -ti clab-muzolab-host10 ip a add 172.0.0.1/31 dev eth1
+docker exec -ti clab-muzolab-host10 ip a add 172.100.0.1/31 dev eth1
 
 docker exec -ti clab-muzolab-host20 ip li set dev eth1 mtu 1500
 docker exec -ti clab-muzolab-host20 ip li set dev eth1 address 00:00:00:01:01:00
-docker exec -ti clab-muzolab-host20 ip a add 172.100.0.1/31 dev eth1
+docker exec -ti clab-muzolab-host20 ip a add 172.200.0.1/31 dev eth1
